48b4a8a79f36d0aa666c02db8e0c25ef6f874fe0
Etiuzale Ikpemhinoghena <etiuzaleikpemhinoghena@gmail.com>
2026-05-28 09:55:53 +0100

    aboki

---
+++ src/models/projects.js
@@ -0,0 +1,33 @@
+import db from './db.js';
+
+const getAllProjects = async() => {
+    const query = `
+        SELECT project_id, organization_id, title, description, location, project_date
+        FROM project;
+    `;
+
+    const result = await db.query(query);
+    return result.rows;
+}
+
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+            project_id,
+            organization_id,
+            title,
+            description,
+            location,
+            project_date
+        FROM project
+        WHERE organization_id = $1
+        ORDER BY project_date;
+    `;
+    
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId };
