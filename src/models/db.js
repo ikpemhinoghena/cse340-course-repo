0f9b4abba7d10bb827fcf3e446838dd7ec507e15
Etiuzale Ikpemhinoghena <etiuzaleikpemhinoghena@gmail.com>
2026-06-11 15:45:54 +0100

    omo work now

--- src/models/db.js
+++ src/models/db.js
@@ -1,4 +1,3 @@
-
 import { Pool } from 'pg';
 
 /**
@@ -14,7 +13,9 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 /**
